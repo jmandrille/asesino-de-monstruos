71544aa11fc22348ef250a0c1da0096b2a3ae02c
Enrisimp <internet_surfer1@yahoo.com>
2020-04-27 16:40:52 -0300

    Ataques y comandos

--- app.js
+++ app.js
@@ -7,9 +7,35 @@ new Vue ({
     },
     methods: {
         comenzarJuego: function() {
-            this.comenzarJuego = true;
+            this.juegoIniciado = true;
             this.jugadorSalud = 100;
             this.monstruoSalud = 100;
-        }
-    }
+        },
+
+        atacar: function () {
+            var dañomax = 10;
+            var dañomin = 3;
+            var daño = Math.floor(Math.random() * dañomax) + dañomin;
+            this.monstruoSalud -= daño;
+      
+        },
+
+        ataqueespecial: function () {
+            var dañomax = 15;
+            var dañomin = 5;
+            var daño = Math.floor(Math.random() * dañomax) + dañomin;
+            this.monstruoSalud -= daño;
+           },
+
+        curar: function () {
+            this.jugadorSalud += 15;
+
+            
+        },
+        
+        rendirse: function () {
+            this.juegoIniciado = false
+          }
+    },
+         
 })
